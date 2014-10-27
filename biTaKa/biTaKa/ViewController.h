2a9b6539035cbcad87685f26c858aefa9df4f2a2
Admin <admin@Mac-Admin.local>
2014-10-27 20:25:18 +0200

    some UI

---
+++ biTaKa/biTaKa/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  biTaKa
+//
+//  Created by Admin on 27/10/14.
+//  Copyright (c) 2014 Admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
