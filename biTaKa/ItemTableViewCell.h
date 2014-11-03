a09b8dac47c07b8915789467589830c11bf62e86
Angel Raev <admin@Mac-Admin.local>
2014-11-03 17:36:58 +0200

    add parse framework

---
+++ biTaKa/ItemTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  ItemTableViewCell.h
+//  biTaKa
+//
+//  Created by Admin on 03/11/14.
+//  Copyright (c) 2014 Admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ItemTableViewCell : UITableViewCell
+
+@end
---
+++ biTaKa/ItemTableViewCell.m
@@ -0,0 +1,23 @@
+//
+//  ItemTableViewCell.m
+//  biTaKa
+//
+//  Created by Admin on 03/11/14.
+//  Copyright (c) 2014 Admin. All rights reserved.
+//
+
+#import "ItemTableViewCell.h"
+
+@implementation ItemTableViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
